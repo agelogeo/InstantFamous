12a3eb90564561e2b3cf6dbc03617d2dbfa62bb8
Giorgos Angelopoulos <agelogeo@gmail.com>
2019-02-20 17:35:18 +0200

    Added a popover for log out Tried new navigation to store tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccountComponent} from './popover/account/account.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: [AppComponent, AccountComponent],
+  entryComponents: [AccountComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import {NavController, PopoverController} from '@ionic/angular';
+import {AccountComponent} from '../popover/account/account.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  constructor(
+      public popoverController: PopoverController,
+      public navCtrl: NavController) {}
+
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: AccountComponent,
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
+
+  goToStore() {
+    this.navCtrl.navigateForward('tabs/tab4');
+  }
+
+
+}
